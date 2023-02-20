1700f6fb2c4ada7722ced669e5449b2efe7cae0b
David Espino <davidespinog@gmail.com>
2023-02-20 17:25:45 -0600

    Add babel configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// import "./style.scss";
+import "./style.scss";
 
 const fancyFunc = () => {
   return [1, 2]
--- webpack.config.js
+++ webpack.config.js
@@ -6,8 +6,13 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      { 
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"]
       }
     ]
   },
