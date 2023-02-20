709cc87243df1c02f1512c8f2a12f7005ef30838
David Espino <davidespinog@gmail.com>
2023-02-20 16:35:02 -0600

    First steps configuring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,5 @@
-const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
 
 module.exports = {
-  entry: {index: path.resolve(__dirname, "src", "index.js")},
-  module : {
-    rules: [
-      {
-        test: /\.scss|css$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src", "index.html")
-    })
-  ], 
-};
+  entry: { index: path.resolve(__dirname, 'src', 'index.js')}
+}
