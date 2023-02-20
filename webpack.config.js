930f0fd960d0f474c675b28837342c41225821bd
David Espino <davidespinog@gmail.com>
2023-02-20 17:15:50 -0600

    Add html and css configuration for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: { index: path.resolve(__dirname, 'src', 'index.js')}
-}
+  entry: { index: path.resolve(__dirname, 'src', 'index.js')},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    })
+  ]
+};
