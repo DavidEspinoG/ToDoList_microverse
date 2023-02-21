07ddb071f2acc256f014cae1606ff1cd175708cf
David Espino <davidespinog@gmail.com>
2023-02-21 16:19:20 -0600

    Add text-decoration : line-through to finished todos

--- src/modules/createTodo.js
+++ src/modules/createTodo.js
@@ -10,18 +10,20 @@ const createTodo = (object) => {
   const left = document.createElement('div');
   left.classList.add('left');
   const square = document.createElement('i');
-  if (!object.completed) {
-    square.classList.add('fa-regular');
-    square.classList.add('fa-square');
-  } else {
-    square.classList.add('fa-solid');
-    square.classList.add('fa-check');
-  }
   square.classList.add('fa-2x');
   const description = document.createElement('input');
   description.value = object.description;
   description.classList.add('description');
   description.tabIndex = -1;
+  if (!object.completed) {
+    square.classList.add('fa-regular');
+    square.classList.add('fa-square');
+    description.classList.remove('finished');
+  } else {
+    description.classList.add('finished');
+    square.classList.add('fa-solid');
+    square.classList.add('fa-check');
+  }
   const dots = document.createElement('i');
   dots.classList.add('fa-solid');
   dots.classList.add('fa-ellipsis-vertical');
